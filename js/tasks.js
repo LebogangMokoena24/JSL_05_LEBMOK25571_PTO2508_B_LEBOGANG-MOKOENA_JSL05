ecd46c31aaf49d7853bf2d03af2dbab5362592f0
Lebogang Mokoena <ditswe24@gmail.com>
2026-03-29 14:48:21 +0200

    Update tasks.js and Create tasks module to manage task state and updates

--- js/tasks.js
+++ js/tasks.js
@@ -1,1 +1,30 @@
+import { saveTasks } from "./storage.js";
 
+/**
+ * Tasks state
+ */
+export let tasks = [];
+
+/**
+ * Set tasks
+ */
+export function setTasks(newTasks) {
+  tasks = newTasks;
+  saveTasks(tasks);
+}
+
+/**
+ * Add task
+ */
+export function addTask(task) {
+  tasks.push(task);
+  saveTasks(tasks);
+}
+
+/**
+ * Update task
+ */
+export function updateTask(updatedTask) {
+  tasks = tasks.map(t => (t.id === updatedTask.id ? updatedTask : t));
+  saveTasks(tasks);
+}
